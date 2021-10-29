f3d0c5e9614984cdc2a9fb256308b9eed1669fbf
Toon Verwerft <toonverwerft@gmail.com>
2021-10-29 07:58:16 +0200

    Add common xmlns namespaces and improve wsdl preset

--- tests/Unit/Xpath/WsdlPresetTest.php
+++ tests/Unit/Xpath/WsdlPresetTest.php
@@ -10,12 +10,48 @@ use function VeeWee\Xml\Dom\Predicate\is_element;
 
 final class WsdlPresetTest extends TestCase
 {
-    public function test_it_provides_a_wsdl_xpath_preset(): void
+    public function test_it_provides_a_wsdl_prefix_in_xpath_preset(): void
     {
         $doc = Document::fromXmlFile(FIXTURE_DIR.'/weather-ws.wsdl');
         $xpath = $doc->xpath(new WsdlPreset($doc));
 
         $definitions = $xpath->querySingle('/wsdl:definitions');
+        static::assertTrue(is_element($definitions));
+    }
+
+    public function test_it_provides_a_soap_prefix_in_xpath_preset(): void
+    {
+        $doc = Document::fromXmlFile(FIXTURE_DIR.'/weather-ws.wsdl');
+        $xpath = $doc->xpath(new WsdlPreset($doc));
+
+        $address = $xpath->querySingle('//soap:address');
+        static::assertTrue(is_element($address));
+    }
+
+    public function test_it_provides_a_soap12_prefix_in_xpath_preset(): void
+    {
+        $doc = Document::fromXmlFile(FIXTURE_DIR.'/weather-ws.wsdl');
+        $xpath = $doc->xpath(new WsdlPreset($doc));
+
+        $address = $xpath->querySingle('//soap12:address');
+        static::assertTrue(is_element($address));
+    }
+
+    public function test_it_provides_a_schema_prefix_in_xpath_preset(): void
+    {
+        $doc = Document::fromXmlFile(FIXTURE_DIR.'/weather-ws.wsdl');
+        $xpath = $doc->xpath(new WsdlPreset($doc));
+
+        $schema = $xpath->querySingle('//schema:schema');
+        static::assertTrue(is_element($schema));
+    }
+
+    public function test_it_provides_a_tns_prefix_in_xpath_preset(): void
+    {
+        $doc = Document::fromXmlFile(FIXTURE_DIR.'/weather-ws.wsdl');
+        $xpath = $doc->xpath(new WsdlPreset($doc));
+
+        $definitions = $xpath->querySingle('//tns:hello');
         static::assertTrue(is_element($definitions));
     }
 }
